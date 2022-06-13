32e54fa72c8ec143bdfd3fd38e869f0a6aad53bb
Danik <chapagana@gmail.com>
2022-06-13 14:25:33 +0600

    added chat

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,11 +1,70 @@
-const Chat =() =>
-{
-    return(
-        <>
-        <div className="chat-display">
+import React, { useEffect, useState } from "react";
 
+function Chat({ socket, username, room }) {
+    const [msg, setMsg] = useState("");
+    const [myChat, setMyChat] = useState([]);
+
+    const sendMsg = async () => {
+        if (msg !== "") {
+            const currentHour = new Date(Date.now()).getHours();
+            const currentMinute=new Date(Date.now()).getMinutes();
+            const msgInfo = {
+                room: room,
+                author: username,
+                message: msg,
+                time: (currentHour<10?"0":"") + currentHour + ":" + (currentMinute<10?"0":"") + currentMinute
+            };
+            await socket.emit("send_message", msgInfo);
+            setMyChat((list) => [...list, msgInfo]);
+            setMsg("");
+        }
+    };
+
+    useEffect(() => {
+        socket.on("get_message", (data) => {
+            setMyChat((list) => [...list, data]);
+        });
+    }, [socket]);
+
+    return (
+        <div className="chat-window">
+            <div className="chat-body">
+                <div className="message-container">
+                    {myChat.map((msgInfo) => {
+                        return (
+                            <div
+                                className="message"
+                                id={username === msgInfo.author ? "you" : "other"}
+                            >
+                                <div>
+                                    <div className="message-content">
+                                        <p>{msgInfo.message}</p>
+                                    </div>
+                                    <div className="message-meta">
+                                        <p id="time">{msgInfo.time}</p>
+                                        <p id="author">{msgInfo.author}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })
+                    
+                    }
+                </div>
+            </div>
+            <div className="chat-footer">
+                <input
+                    type="text"
+                    value={msg}
+                    placeholder="Hey..."
+                    onChange={(event) => {
+                        setMsg(event.target.value);
+                    }}
+                />
+                <button onClick={sendMsg}>&#9658;</button>
+            </div>
         </div>
-        </>
-    )
+    );
 }
-export default Chat
+
+export default Chat;
